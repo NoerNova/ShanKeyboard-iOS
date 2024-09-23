bfaf3b872d7c10827a142fe6c5cbf5638a3f4e09
NoerNova <noernova666@gmail.com>
2024-09-24 02:51:11 +0700

    Remove unwanted: remove unwanted code

--- Panglong/LayoutServiceProvider.swift
+++ Panglong/LayoutServiceProvider.swift
@@ -39,48 +39,4 @@ class LayoutServiceProvider: KeyboardLayout.BaseService, LocalizedService {
         default: iPhoneService
         }
     }
-    
-    
-//    let extraKey: ExtraKey
-//    
-//    enum ExtraKey {
-//        case none
-//        case emojiIfNeeded
-//        case keyboardSwitcher
-//        case url(String)
-//    }
-    
-//    override func keyboardLayout(for context: KeyboardContext) -> KeyboardLayout {
-//        let layout = super.keyboardLayout(for: context)
-//        
-//        switch extraKey {
-//        case .none:
-//            break
-//        case .emojiIfNeeded:
-//            layout.tryInsertEmojiButton()
-//        case .keyboardSwitcher:
-//            layout.tryInsert(.nextKeyboard)
-//        case .url(let string):
-//            layout.tryInsert(.url(.init(string: string), id: nil))
-//        }
-//        
-//        return layout
-//    }
-
 }
-
-//private extension KeyboardLayout {
-//    func tryInsert(_ action: KeyboardAction) {
-//        guard let item = tryCreateBottomRowItem(for: action) else { return }
-//        itemRows.insert(item, before: .space, atRow: bottomRowIndex)
-//    }
-//    
-//    func tryInsertEmojiButton() {
-//        guard let row = bottomRow else { return }
-//        let hasEmoji = row.contains(where: { $0.action == .keyboardType(.emojis) })
-//        if hasEmoji { return }
-//        guard let button = tryCreateBottomRowItem(for: .keyboardType(.emojis)) else { return }
-//        itemRows.insert(button, after: .space, atRow: bottomRowIndex)
-//    }
-//}
-
