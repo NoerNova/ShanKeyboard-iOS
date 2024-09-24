31877ee63eafb51a95ee69cbae74f7689a4498b6
NoerNova <noernova666@gmail.com>
2024-09-24 23:17:18 +0700

    Bug fixed: fixed dictionaryWords not load

--- Panglong/Autocomplete/Tokenizer/Tokeinzer.swift
+++ Panglong/Autocomplete/Tokenizer/Tokeinzer.swift
@@ -10,39 +10,32 @@ import Foundation
 public class Tokenizer {
     
     init() {
-        Tokenizer.loadDictionaryWords()
-    }
-    
-    private static var dictionaryWords: Set<String> = []
-    
-    // This is the tokenize function that can be used globally
-    public static func tokenize(_ text: String) -> [String] {
-        var words: [String] = []
-        var remainingText = text
-        
-        while !remainingText.isEmpty {
-            if let word = findLongestMatch(in: remainingText) {
-                words.append(word)
-                remainingText = String(remainingText.dropFirst(word.count))
-            } else {
-
-                words.append(String(remainingText.prefix(1)))
-                remainingText = String(remainingText.dropFirst())
-            }
-        }
-        
-        return words
-    }
-    
-    private static func loadDictionaryWords() {
         if let path = Bundle.main.path(forResource: "dictionary", ofType: "txt"),
            let content = try? String(contentsOfFile: path) {
             dictionaryWords = Set(content.components(separatedBy: .newlines))
         }
     }
     
+    private var dictionaryWords: Set<String> = []
+    
+    // This is the tokenize function that can be used globally
+    public func tokenize(_ text: String) -> [String] {
+        var words: [String] = []
+        var remainingText = text
+        while !remainingText.isEmpty {
+            if let word = findLongestMatch(in: remainingText) {
+                words.append(word)
+                remainingText = String(remainingText.dropFirst(word.count))
+            } else {
+                words.append(String(remainingText.prefix(1)))
+                remainingText = String(remainingText.dropFirst())
+            }
+        }
+        return words
+    }
+    
     // Helper method to find the longest match in the dictionary
-    private static func findLongestMatch(in text: String) -> String? {
+    private func findLongestMatch(in text: String) -> String? {
         for endIndex in (1...text.count).reversed() {
             let substring = String(text.prefix(endIndex))
             if dictionaryWords.contains(substring) {
