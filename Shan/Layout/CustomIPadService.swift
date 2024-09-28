c6235ff1b420ef46e5d9f5451b87d6a8e25cb9ea
NoerNova <noernova666@gmail.com>
2024-09-29 04:29:29 +0700

    iPad: initial Layout for iPad

--- Shan/Layout/CustomIPadService.swift
+++ Shan/Layout/CustomIPadService.swift
@@ -7,4 +7,51 @@
 
 import KeyboardKit
 
-class CustomIPadService: KeyboardLayout.iPadService {}
+class CustomIPadService: KeyboardLayout.iPadService {
+    
+    /// The bottom row actions.
+    override func bottomActions(for context: KeyboardContext) -> KeyboardAction.Row {
+        var result = KeyboardAction.Row()
+        let needsDictation = context.needsInputModeSwitchKey
+        
+        result.append(.nextKeyboard)
+        if let action = keyboardSwitchActionForBottomRow(for: context) { result.append(action) }
+        if needsDictation, let action = context.keyboardDictationReplacement { result.append(action) }
+        result.append(.space)
+        if let action = keyboardSwitchActionForBottomRow(for: context) { result.append(action) }
+        result.append(.dismissKeyboard)
+        return result
+    }
+    
+    open override func itemSizeWidth(
+        for action: KeyboardAction,
+        row: Int,
+        index: Int,
+        context: KeyboardContext
+    ) -> KeyboardLayout.ItemWidth {
+        if isLowerTrailingSwitcher(action, row: row, index: index) { return .available }
+        switch action {
+        case context.keyboardDictationReplacement: return .input
+        case .none: return .inputPercentage(0.0001)
+        case .primary: return .available
+        case .backspace: return .inputPercentage(1.6)
+        case .shift: return .inputPercentage(1.6225)
+        default: break
+        }
+        if action.isSystemAction { return systemButtonWidth(for: context) }
+        return super.itemSizeWidth(for: action, row: row, index: index, context: context)
+    }
+}
+
+extension CustomIPadService {
+    func isLowerTrailingSwitcher(
+        _ action: KeyboardAction,
+        row: Int,
+        index: Int
+    ) -> Bool {
+        switch action {
+        case .shift, .keyboardType: row == 2 && index > 0
+        default: false
+        }
+    }
+}
