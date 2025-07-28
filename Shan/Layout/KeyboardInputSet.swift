c210430059e7c73ee1830399662dc2e107511071
NoerNova <noernova666@gmail.com>
2025-07-28 13:34:57 +0700

    SIL: add SIL Shan Standard input layout set

--- Shan/Layout/KeyboardInputSet.swift
+++ Shan/Layout/KeyboardInputSet.swift
@@ -8,6 +8,7 @@
 import KeyboardKit
 
 public extension InputSet {
+    // MARK: - Panglong Layout
     static var panglong: InputSet {
         .init(rows: [
             .init(lowercased: ["ၸ", "တ", "ၼ", "မ", "ႄ", "ပ", "ၵ", "င", "သ", "ၺ"], uppercased: ["ၹ", "ၻ", "ꧣ", "႞", "ၿ", "ြ", "ၷ", "ႀ", "ဝ", "ႁ"]),
@@ -16,7 +17,7 @@ public extension InputSet {
         ])
     }
     
-    static var shanNumeric: InputSet {
+    static var panglongNumeric: InputSet {
         .init(rows: [
             .init(chars: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]),
             .init(phone: "-/:;()$&@”", pad: "@#$&*()’”"),
@@ -24,7 +25,34 @@ public extension InputSet {
         ])
     }
     
-    static func shanSymbolic(currencies: [String]) -> InputSet {
+    static func panglongSymbolic(currencies: [String]) -> InputSet {
+        .init(rows: [
+            .init(phone: "[]{}#%^*+=", pad: "႑႒႓႔႕႖႗႘႙႐"),
+            .init(
+                phone: "_\\|~<>\(currencies.joined())•",
+                pad: "\(currencies.joined())_^[]{}"),
+            .init(phone: ".,?!’", pad: "§|~…\\<>!?")
+        ])
+    }
+    
+    // MARK: - Shan Standard Layout
+    static var shanSIL: InputSet {
+        .init(rows: [
+            .init(lowercased: ["ၸ", "တ", "ၼ", "မ", "ဢ", "ပ", "ၵ", "င", "ဝ", "ႁ"], uppercased: ["ꩡ", "ၻ", "ꧣ", "႞", "ြ", "ၿ", "ၷ", "ရ", "သ", "ႀ"]),
+            .init(lowercased: ["ေ", "ႄ", "ိ", "်", "ွ", "ႉ", "ႇ", "ု", "ူ", "ႈ"], uppercased: ["ဵ", "ႅ", "ီ", "ႂ်", "ႂ", "့", "ႆ", "\"", "ႊ", "း"]),
+            .init(lowercased: ["ၽ", "ထ", "ၶ", "လ", "ယ", "ၺ", "ၢ", "။"], uppercased: ["ၾ", "ꩪ", "ꧠ", "ꩮ", "ျ", "႟", "ႃ", "ꧦ"])
+        ])
+    }
+    
+    static var shanSILNumeric: InputSet {
+        .init(rows: [
+            .init(chars: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]),
+            .init(phone: "-/:;()$&@”", pad: "@#$&*()’”"),
+            .init(phone: ".,?!’၊။", pad: "%-+=/;:!?")
+        ])
+    }
+    
+    static func shanSILSymbolic(currencies: [String]) -> InputSet {
         .init(rows: [
             .init(phone: "[]{}#%^*+=", pad: "႑႒႓႔႕႖႗႘႙႐"),
             .init(
