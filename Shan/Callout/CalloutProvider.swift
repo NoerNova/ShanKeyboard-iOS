73d50f450dbb71c0eaf77a6a3899f49d5d1b8315
NoerNova <noernova666@gmail.com>
2025-07-28 13:35:32 +0700

    Callout Action: add more callout action characters

--- Shan/Callout/CalloutProvider.swift
+++ Shan/Callout/CalloutProvider.swift
@@ -13,16 +13,23 @@ class CalloutProvider: Callouts.BaseService {
         switch action {
         case .character(let char):
             switch char {
-            case "1": return "1႑".chars.map { KeyboardAction.character($0)}
-            case "2": return "2႒".chars.map { KeyboardAction.character($0)}
-            case "3": return "3႓".chars.map { KeyboardAction.character($0)}
-            case "4": return "4႔".chars.map { KeyboardAction.character($0)}
-            case "5": return "5႕".chars.map { KeyboardAction.character($0)}
-            case "6": return "6႖".chars.map { KeyboardAction.character($0)}
-            case "7": return "7႗".chars.map { KeyboardAction.character($0)}
-            case "8": return "8႘".chars.map { KeyboardAction.character($0)}
-            case "9": return "9႙".chars.map { KeyboardAction.character($0)}
-            case "0": return "0႐°".chars.map { KeyboardAction.character($0)}
+            case "1": return "1၁႑".chars.map { KeyboardAction.character($0)}
+            case "2": return "2၂႒".chars.map { KeyboardAction.character($0)}
+            case "3": return "3၃႓".chars.map { KeyboardAction.character($0)}
+            case "4": return "4၄႔".chars.map { KeyboardAction.character($0)}
+            case "5": return "5၅႕".chars.map { KeyboardAction.character($0)}
+            case "6": return "6၆႖".chars.map { KeyboardAction.character($0)}
+            case "7": return "7၇႗".chars.map { KeyboardAction.character($0)}
+            case "8": return "8၈႘".chars.map { KeyboardAction.character($0)}
+            case "9": return "9၉႙".chars.map { KeyboardAction.character($0)}
+            case "0": return "0၀႐°".chars.map { KeyboardAction.character($0)}
+            case "ꩡ": return "ꩡၹ".chars.map { KeyboardAction.character($0)}
+            case "ၻ": return "ၻꩦꩨ".chars.map { KeyboardAction.character($0)}
+            case "သ": return "သႀ︀".chars.map { KeyboardAction.character($0)}
+            case "ႅ": return "ႅꧥ".chars.map { KeyboardAction.character($0)}
+            case "ၾ": return "ၾꧤ".chars.map { KeyboardAction.character($0)}
+            case "ꩪ": return "ꩪꩧ".chars.map { KeyboardAction.character($0)}
+            case "။": return "။၊".chars.map { KeyboardAction.character($0)}
             default: break
             }
         default: break
